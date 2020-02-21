798e346e1cd04e210b5948e4232d9c98d1ae51ae
Axel Velazquez Merino <gafph02@outlook.com>
2020-02-20 20:50:10 -0600

    wa_Cuestionarios_dev0001

---
+++ test.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="wa_Cuestionarios.test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <link href="Content/fontawesome-free-5.12.1-web/css/all.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <br />
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                    <asp:LinkButton ID="LinkButton1" runat="server" CssClass="btn btn-primary form-control" OnClick="LinkButton1_Click">
+                                <i class="fas fa-save"></i> 
+                    </asp:LinkButton>
+                </div>
+                <div class="col-sm">
+                </div>
+                <div class="col-sm">
+                </div>
+            </div>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ test.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace wa_Cuestionarios
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            int int_FiltroMateriaID;
+            int int_FiltroMateriaTemaID;
+            int int_CantidadPreguntas;
+            int int_TipoPreguntas;
+
+
+
+            // Save to session state in a Web Forms page class.
+            Session["Sess_FiltroMateriaID"] = 2;
+            Session["Sess_FiltroMateriaTemaID"] = 13;
+            Session["Sess_TipoPreguntas"] = 1;
+            Session["Sess_CantidadPreguntas"] = 5;
+
+
+            // Read from session state in a Web Forms page class.
+            int_FiltroMateriaID = (int)(Session["Sess_FiltroMateriaID"]);
+            int_FiltroMateriaTemaID = (int)(Session["Sess_FiltroMateriaTemaID"]);
+            int_TipoPreguntas = (int)(Session["Sess_TipoPreguntas"]);
+        
+
+            if (int_TipoPreguntas == 1)
+            {
+                int_CantidadPreguntas = (int)(Session["Sess_CantidadPreguntas"]);
+            }
+            else
+            {
+
+                using (imDesarrolloEntities db = new imDesarrolloEntities())
+                {
+                    var art = db.catMateriaTemaPreguntaRespuestaBitacora
+                                .Join(db.Famiglie,
+                                      articolo => articolo.CodFamiglia,
+                                      famiglia => famiglia.CodFamiglia,
+                                      (articolo, famiglia) => new { Articoli = articolo, Famiglie = famiglia })
+                                .Where((x) => x.Articoli.CodArt == "ART005")
+                                .FirstOrDefault();
+
+                    MessageBox.Show(art.Articoli.DesArt + " - " + art.Famiglie.DesFamiglia);
+                }
+
+                int_CantidadPreguntas = (int)(Session["Sess_CantidadPreguntas"]);
+            }
+
+
+        }
+    }
+}
